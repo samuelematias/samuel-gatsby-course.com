2d113396ef073a93dbe05fe26170df144545e91a
samuelematias <hi@samuelematias.com>
2019-12-31 17:56:04 -0300

    Create and add aside on layout.

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Samuel Matias</h1>
+        <h2>Software Engineer</h2>
+        <p>Revolutionising the Universe of Food at iFood</p>
+    </div>
+)
+
+export default Profile
