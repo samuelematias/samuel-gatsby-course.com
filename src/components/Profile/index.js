406506f04ce459411c121f7f64ad810047941432
samuelematias <hi@samuelematias.com>
2019-12-31 18:42:55 -0300

    Added Prettier

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,31 +1,32 @@
-import React from 'react'
+import React from "react"
 
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql } from "gatsby"
 
 const Profile = () => (
-    <StaticQuery
-        query={graphql`
-        query MySiteMetadata {
-            site {
-              siteMetadata {
-                title
-                position
-                description
-              }
-            }
-          }   
+  <StaticQuery
+    query={graphql`
+      query MySiteMetadata {
+        site {
+          siteMetadata {
+            title
+            position
+            description
+          }
+        }
+      }
     `}
-        render={({ site: { siteMetadata: { title, position, description } } }) => (
-
-            <div className="Profile-wrapper">
-                <h1>{title}</h1>
-                <h2>{position}</h2>
-                <p>{description}</p>
-            </div>
-
-        )}
-    />
-
+    render={({
+      site: {
+        siteMetadata: { title, position, description },
+      },
+    }) => (
+      <div className="Profile-wrapper">
+        <h1>{title}</h1>
+        <h2>{position}</h2>
+        <p>{description}</p>
+      </div>
+    )}
+  />
 )
 
 export default Profile
