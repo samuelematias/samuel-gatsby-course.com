f59012d5a24c47da660931c2a3c82cd0d4bce2cf
samuelematias <hi@samuelematias.com>
2019-12-31 17:12:43 -0300

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Samuel Matias`,
+    description: `A blog about tech, esports, diversity, music and cool stuffs.`,
+    author: `@samuelematias`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
