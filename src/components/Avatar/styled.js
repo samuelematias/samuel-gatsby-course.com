438580bfde75a982fc42419596b93c050f98119f
samuelematias <hi@samuelematias.com>
2019-12-31 21:30:37 -0300

    Separated the styles(styled) from the Avatar comp.

---
+++ src/components/Avatar/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+export const AvatarWrapper = styled(Img)`
+  border-radius: 50%;
+  width: 3.75rem;
+  height: 3.75rem;
+  margin: auto;
+`
