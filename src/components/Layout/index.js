1ff4f9f594f5a31358eb493904f7f29b1a85402f
samuelematias <hi@samuelematias.com>
2019-12-31 21:59:20 -0300

    removed unnecessary import

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Profile from "../Profile"
 import GlobalStyles from "../../styles/global"
 import * as S from "./styled"
 import Sidebar from "../Sidebar"
