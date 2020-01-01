8faa7f9d18ba52c6bd1ec52fbb63471f3f138acb
samuelematias <hi@samuelematias.com>
2020-01-01 12:42:36 -0300

    Implemented the PostItem by props.

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,23 +1,39 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 import * as S from "./styled"
 
-const PostItem = () => (
-  <S.PostItemLink>
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#47650b">Misc</S.PostItemTag>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
         <S.PostItemDate>
-          27 de Dezembro de 2019 • 9 min de leitura
+          {date} • {timeToRead} min de leitura
         </S.PostItemDate>
-        <S.PostItemTitle>Meu ano de 2019</S.PostItemTitle>
-        <S.PostItemDescription>
-          Um ano movimentado na Toptal, blog novo, curso novo e viagens como
-          sempre.
-        </S.PostItemDescription>
+        <S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.prototype = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
 export default PostItem
