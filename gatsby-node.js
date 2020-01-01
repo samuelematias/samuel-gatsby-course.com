aa3146296768cc4bde7a7f831a197a065e819fab
samuelematias <hi@samuelematias.com>
2020-01-01 14:00:00 -0300

    Update the createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,9 +22,9 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
+// To create the posts pages
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-
   return graphql(`
     {
       allMarkdownRemark {
@@ -40,9 +40,11 @@ exports.createPages = ({ graphql, actions }) => {
   `).then(result => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
-        patch: node.fields.slug,
-        component: path.resolve("./src/templates/blog-post.js"),
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
         context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
           slug: node.fields.slug,
         },
       })
