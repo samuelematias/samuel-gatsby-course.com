daf89d9c04093b3417fd12869abd681e3ac6032d
samuelematias <hi@samuelematias.com>
2020-01-02 15:01:43 -0300

    Bg color by Dark/Light Mode on RecommendPost

--- src/components/RecommendedPosts/index.js
+++ src/components/RecommendedPosts/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import propTypes from "prop-types"
 
+import getThemeColor from "../../utils/getThemeColor"
+
 import * as S from "./styled"
 
 const RecommendedPosts = ({ next, previous }) => (
@@ -10,7 +12,7 @@ const RecommendedPosts = ({ next, previous }) => (
         to={previous.fields.slug}
         cover
         direction="left"
-        bg="#16202c"
+        bg={getThemeColor()}
         className="previous"
       >
         {previous.frontmatter.title}
@@ -21,7 +23,7 @@ const RecommendedPosts = ({ next, previous }) => (
         to={next.fields.slug}
         cover
         direction="right"
-        bg="#16202c"
+        bg={getThemeColor()}
         className="next"
       >
         {next.frontmatter.title}
