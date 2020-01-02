ae5121d71ac46a34c98e700444f0b6f82382323a
samuelematias <hi@samuelematias.com>
2020-01-02 18:48:53 -0300

    Update samuelematias url's for https

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,7 +5,7 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-  const completeURL = `http://samuelematias.com${url}`
+  const completeURL = `https://samuelematias.com/${url}`
 
   return (
     <S.CommentsWrapper>
