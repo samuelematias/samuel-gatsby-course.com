a725b887c085b1a9a5fef8339008411f0a1a5f14
samuelematias <hi@samuelematias.com>
2020-01-02 17:02:31 -0300

    Config the Manifest for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,11 +74,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Samuel Matias`,
+        short_name: `Samuel Matias`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
