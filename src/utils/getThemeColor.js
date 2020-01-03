2701e2c33e81c680fcb8305b0376a1982d77a7f1
samuelematias <hi@samuelematias.com>
2020-01-03 19:08:33 -0300

    Implemented color var on getThemeColor for Dark

--- src/utils/getThemeColor.js
+++ src/utils/getThemeColor.js
@@ -1,8 +1,10 @@
+import * as V from "../styles/variables"
+
 const getThemeColor = () => {
   const theme = typeof window !== "undefined" && window.__theme
 
   if (theme === "light") return "#fff"
-  if (theme === "dark") return "#16202c"
+  if (theme === "dark") return V.Color.grayDarker
 }
 
 export default getThemeColor
