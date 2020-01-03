80f15ab2efc1f657ad598838df1e6759e264bcf4
samuelematias <hi@samuelematias.com>
2020-01-03 17:44:24 -0300

    Implelenting transitions on RecommendedPosts comp

--- src/components/RecommendedPosts/styled.js
+++ src/components/RecommendedPosts/styled.js
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import media from "styled-media-query"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
+import transitions from "../../styles/transitions"
+
 export const RecommendedWrapper = styled.section`
   border-bottom: 1px solid var(--borders);
   border-top: 1px solid var(--borders);
@@ -16,29 +18,39 @@ export const RecommendedLink = styled(AniLink)`
   display: flex;
   padding: 3rem;
   text-decoration: none;
-  transition: background 0.5s;
+  transition: ${transitions.BACKGROUND};
   width: 50%;
-  &:hover {
-    background: var(--borders);
-  }
-  &.previous {
-    border-right: 1px solid var(--borders);
-  }
-  &.next {
-    justify-content: flex-end;
-  }
-  &.previous:before {
-    content: "\\2190";
-    margin-right: 0.5rem;
-  }
-  &.next:after {
-    content: "\\2192";
-    margin-left: 0.5rem;
-  }
 
   ${media.lessThan("large")`
     padding: 2rem 1rem;
     line-height: 1.3;
     font-size: .9rem;
   `}
+
+  &:hover {
+    background: var(--borders);
+  }
+
+  &.previous {
+    border-right: 1px solid var(--borders);
+  }
+
+  &.next {
+    justify-content: flex-end;
+  }
+
+  &.next:only-child {
+    margin-left: auto;
+    border-left: 1px solid var(--borders);
+  }
+
+  &.previous:before {
+    content: "\\2190";
+    margin-right: 0.5rem;
+  }
+
+  &.next:after {
+    content: "\\2192";
+    margin-left: 0.5rem;
+  }
 `
